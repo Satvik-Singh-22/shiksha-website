160195769fe44b18bc11e7808c5489eb27c9271c
Satvik Singh <singhsatvik2207@gmail.com>
2025-06-20 14:18:31 +0530

    Donation page updated

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const siteConfigs= {
+    siteTitle : "Shiksha Abhiyan Educational Foundation",
+    siteAltTitle : "Shiksha Abhiyan NGO",
+    contactEmail: "Contactngo3@gmail.com",
+    contactNumber1: "9911769755",
+    contactNumber2: "",
+    ngoAddress: "Mtnl, Block A, Vijay Enclave, Dabri, New Delhi, Delhi, 110046"
+}
+
+const websiteLinks= {
+    volunteerForm: "https://docs.google.com/forms/d/e/1FAIpQLSfI9lNqHRFcr_aDrFpEAbmXVG6QuV6JtSBXzyTj0U8LxT875Q/viewform?usp=dialog"
+}
